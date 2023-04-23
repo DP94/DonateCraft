1bce781fc8b9bf69c2ab9b99c995d4653454bd0f
DP94 <d.pell1994@gmail.com>
2023-04-23 19:50:59 +0100

    Update pr.yml

--- src/Cloud/Terraform/main.tf
+++ src/Cloud/Terraform/main.tf
@@ -7,11 +7,7 @@
   }
   required_version = ">= 1.1.0"
   
-  backend "s3" {
-    bucket = "donatecraft-terraform"
-    key = var.donate_craft_version
-    region = var.aws_region
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
