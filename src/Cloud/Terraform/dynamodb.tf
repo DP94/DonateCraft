857e9495b8e61b172328b888fb07ab1da2aad3c2
DP94 <d.pell1994@gmail.com>
2023-04-30 17:15:11 +0100

    Dynamodb tables, api gateway

---
+++ src/Cloud/Terraform/dynamodb.tf
@@ -0,0 +1,38 @@
+﻿resource "aws_dynamodb_table" "player" {
+  name           = "Player-${var.donate_craft_version}"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "lock" {
+  name           = "Lock-${var.donate_craft_version}"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "charity" {
+  name           = "Charity-${var.donate_craft_version}"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
--- src/Cloud/Terraform/main.tf
+++ src/Cloud/Terraform/main.tf
@@ -17,16 +17,3 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-
-resource "aws_dynamodb_table" "players" {
-  name           = "Player${var.donate_craft_version}"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "id"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-}
