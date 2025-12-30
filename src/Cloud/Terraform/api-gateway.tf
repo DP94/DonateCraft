69cffbfd1b693d0f32a0f1fd65a7457c5b38c17c
DP94 <d.pell1994@gmail.com>
2025-12-30 19:16:34 +0000

    Clean up api-gateway.tf

--- src/Cloud/Terraform/api-gateway.tf
+++ src/Cloud/Terraform/api-gateway.tf
@@ -4,64 +4,64 @@
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.donatecraft.id}"
-  parent_id   = "${aws_api_gateway_rest_api.donatecraft.root_resource_id}"
+  rest_api_id = aws_api_gateway_rest_api.donatecraft.id
+  parent_id   = aws_api_gateway_rest_api.donatecraft.root_resource_id
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.donatecraft.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  rest_api_id   = aws_api_gateway_rest_api.donatecraft.id
+  resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.donatecraft.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.donatecraft.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.donatecraft.invoke_arn}"
+  uri                     = aws_lambda_function.donatecraft.invoke_arn
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.donatecraft.id}"
-  resource_id   = "${aws_api_gateway_rest_api.donatecraft.root_resource_id}"
+  rest_api_id   = aws_api_gateway_rest_api.donatecraft.id
+  resource_id   = aws_api_gateway_rest_api.donatecraft.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.donatecraft.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.donatecraft.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.donatecraft.invoke_arn}"
+  uri                     = aws_lambda_function.donatecraft.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "example" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.donatecraft.id}"
+  rest_api_id = aws_api_gateway_rest_api.donatecraft.id
 }
 
 resource "aws_api_gateway_stage" "example" {
   deployment_id = aws_api_gateway_deployment.example.id
-  rest_api_id = "${aws_api_gateway_rest_api.donatecraft.id}"
+  rest_api_id = aws_api_gateway_rest_api.donatecraft.id
   stage_name  = "donatecraft${var.donate_craft_environment}"
 }
 
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.donatecraft.function_name}"
+  function_name = aws_lambda_function.donatecraft.function_name
   principal     = "apigateway.amazonaws.com"
 
   # The /*/* portion grants access from any method on any resource
