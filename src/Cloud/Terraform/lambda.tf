890697952bfa282e63b0d31e7d7b0daff80e1bf9
DP94 <d.pell1994@gmail.com>
2025-12-30 16:16:44 +0000

    Refactor lambda iam to remove hardcoded role

--- src/Cloud/Terraform/lambda.tf
+++ src/Cloud/Terraform/lambda.tf
@@ -7,7 +7,7 @@
   handler = "Web::Web.LambdaEntryPoint::FunctionHandlerAsync"
   runtime = "dotnet10"
 
-  role = "arn:aws:iam::043470831800:role/donatecraft-AspNetCoreFunctionRole-JZ3T6L7H5ZXQ"
+  role = aws_iam_role.lambda_role.arn
   
   timeout = 60
   memory_size = 512
