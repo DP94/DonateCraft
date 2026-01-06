ed37d07c23569863f4287232b81782494d240d24
DP94 <d.pell1994@gmail.com>
2026-01-06 20:57:18 +0000

    Fixes

--- src/RevivalLambda/Function.cs
+++ src/RevivalLambda/Function.cs
@@ -1,12 +1,9 @@
-using System.Text.Json.Serialization;
+using System;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
-using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Common.Models;
-
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace RevivalLambda;
 
