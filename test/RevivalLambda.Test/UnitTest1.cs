621fd318667a66d3475f9b9df0ce8e949d6ed7e0
DP94 <d.pell1994@gmail.com>
2026-01-07 20:45:49 +0000

    tests

---
+++ test/RevivalLambda.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.Lambda.SQSEvents;
+using NUnit.Framework;
+
+namespace RevivalLambda.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        new Function().HandleRequest(new SQSEvent
+        {
+            Records =
+            [
+                new SQSEvent.SQSMessage
+                {
+                    Body = @"{""donationId"":""1500390957"",""playerId"":""5ba92742-af9d-4ad6-a5a7-c768dd9bc747""}"
+                }
+            ]
+        });
+    }
+}
