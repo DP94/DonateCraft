3521941d059f17e9a9d4b79a79ddead87c7bc435
DP94 <d.pell1994@gmail.com>
2026-01-07 19:57:54 +0000

    Update lambda.tf

--- src/Cloud/Terraform/lambda.tf
+++ src/Cloud/Terraform/lambda.tf
@@ -46,6 +46,7 @@ resource "aws_lambda_function" "revival_lambda" {
       DonateCraft__JustGivingApiUrl = var.just_giving_api_url
       DonateCraft__PlayerTableName  = aws_dynamodb_table.player.name
       DonateCraft__LockTableName    = aws_dynamodb_table.lock.name
+      DonateCraft__CharityTableName = aws_dynamodb_table.charity.name
     }
   }
 }
