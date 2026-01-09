193d0e3a9a510d003d308d4dd25e5e4c1096ee03
DP94 <d.pell1994@gmail.com>
2026-01-09 21:09:35 +0000

    fix

--- src/Cloud/Terraform/sqs.tf
+++ src/Cloud/Terraform/sqs.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "revival_queue" {
   name = "revival-queue${var.donate_craft_environment}"
-  visibility_timeout_seconds = 300,
+  visibility_timeout_seconds = 300
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.revival_dlq.arn
     maxReceiveCount     = 4
